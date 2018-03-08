258616fef2e853251a74c136e535e6b2420d80ad
Monika Ravi <monikar@hawsco.com>
2018-03-08 10:57:18 -0800

    to add server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
